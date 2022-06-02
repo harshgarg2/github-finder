5c7d2874cf43892a768652c1fd1de35617419ef6
harsh garg <harshgarg2>
2022-06-02 14:08:35 +0530

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+       <strong>A React app to search GitHub profiles and see profile details. </strong> </p>
+      <p className='text-lg text-black-400'>
+        Layout By: Harsh Garg
+        
+      </p>
+    </>
+  )
+}
+
+export default About
